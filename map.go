0fb4bdae846f750f097ccd2e52cf1372b355d6b8
MrMelon54 <github@mrmelon54.com>
2024-08-28 00:39:22 +0100

    Oops

--- map.go
+++ map.go
@@ -45,7 +45,7 @@ func (m *Map[K, V]) Range(f func(key K, value V) bool) {
 }
 
 func (m *Map[K, V]) Store(key K, value V) {
-	m.Store(key, value)
+	m.items.Store(key, value)
 }
 
 func (m *Map[K, V]) Swap(key K, value V) (previous V, loaded bool) {
